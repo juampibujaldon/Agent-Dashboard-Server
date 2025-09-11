b540490bb4b2ed6e4447f6018596461ffa044309
Joaquín Caparros <105978542+JoaquinCaparros711@users.noreply.github.com>
2025-09-11 09:44:50 -0300

    #1 Delete agent/target directory

--- agent/target/debug/deps/agent-58098c12e56e8476.d
+++
@@ -1,5 +0,0 @@
-C:\Users\menga\Documents\GitHub\Agent-Dashboard-Server\agent\target\debug\deps\agent-58098c12e56e8476.d: src\main.rs
-
-C:\Users\menga\Documents\GitHub\Agent-Dashboard-Server\agent\target\debug\deps\libagent-58098c12e56e8476.rmeta: src\main.rs
-
-src\main.rs:
--- agent/target/debug/deps/agent.d
+++
@@ -1,5 +0,0 @@
-C:\Users\menga\Documents\GitHub\Agent-Dashboard-Server\agent\target\debug\deps\agent.d: src\main.rs
-
-C:\Users\menga\Documents\GitHub\Agent-Dashboard-Server\agent\target\debug\deps\agent.exe: src\main.rs
-
-src\main.rs:
