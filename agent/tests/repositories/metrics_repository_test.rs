b20a3f22f5085a218ab9af4b9d70fab22a5503f8
Joaquín Caparros <105978542+JoaquinCaparros711@users.noreply.github.com>
2025-10-02 18:30:09 -0300

    #8 Reorder and format test module imports
    Reordered the imports in agent/tests/mod.rs for consistency and fixed formatting. Cleaned up formatting in metrics_repository_test.rs and ensured all test modules are properly imported in repositories/mod.rs.

--- agent/tests/repositories/metrics_repository_test.rs
+++ agent/tests/repositories/metrics_repository_test.rs
@@ -2,22 +2,13 @@ use agent::models::metrics::{Metric, MetricCategory};
 use agent::repositories::metrics_repository::MetricsRepository;
 
 #[tokio::test]
-async fn test_repository_creation(){
-    let repository= MetricsRepository::new();
+async fn test_repository_creation() {
+    let repository = MetricsRepository::new();
 
-    let metric = Metric::new(
-        "CPU Usage",
-        75.5,
-        "%",
-        "server_1",
-        MetricCategory::CPU,
-    );
+    let metric = Metric::new("CPU Usage", 75.5, "%", "server_1", MetricCategory::CPU);
 
-    let saved_metric = repository.create(metric)
-    .await.unwrap();
+    let saved_metric = repository.create(metric).await.unwrap();
 
     assert!(saved_metric.id.is_some());
     assert_eq!(saved_metric.name, "CPU Usage");
-
 }
-
